09da2ba82c7110dc8d9787072a17852679c8e069
Patricia Paavilainen <patricia.paavilainen@eng.tamk.fi>
2019-02-09 18:33:42 +0200

    FINAL

---
+++ eduskuntatulokset/src/frontend/build/precache-manifest.e55cc741dfa5d837cc0d9ee6befcb524.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac7c124e85629603e588b1b4168f9f3a",
+    "url": "/static/media/paasit.ac7c124e.svg"
+  },
+  {
+    "revision": "5bb8b92cdfb1e4bd93b564ee0b0a9164",
+    "url": "/static/media/etusivu.5bb8b92c.svg"
+  },
+  {
+    "revision": "d14cd80ef46cfb3b2a87cd4d352f1fa9",
+    "url": "/static/media/et_paassyt.d14cd80e.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38c91a279a9b2721073d",
+    "url": "/static/js/main.38c91a27.chunk.js"
+  },
+  {
+    "revision": "3080dc9dd2a1ce0b3e6b",
+    "url": "/static/js/1.3080dc9d.chunk.js"
+  },
+  {
+    "revision": "38c91a279a9b2721073d",
+    "url": "/static/css/main.2adf88bd.chunk.css"
+  },
+  {
+    "revision": "5dbad7b3334910e3c05bf4101bbf7f0b",
+    "url": "/index.html"
+  }
+];
